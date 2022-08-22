afa7d3111221df38cf233709a7452d1d6c32d16a
Mateus <correia.mateus@hotmail.com>
2022-08-22 17:58:05 -0300

    Adjusted ball angle

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,6 +7,12 @@ public class BallController : MonoBehaviour {
     [SerializeField] private float _sensitivity;
 
     private void RandomInitialTrajectory() {
+        float random = Random.Range(-80f, 80f);
+
+        while(random > -10f && random < -10f) {
+            random = Random.Range(-80f, 80f);
+        }
+
         transform.rotation = Quaternion.Euler(0, 0, Random.Range(-80f, 80f));
         _ballRB.AddForce(transform.up * _sensitivity);
     }
