148908d9eab39d2ea9b57d2cfd5bbc6cb9fdbb40
Mateus <correia.mateus@hotmail.com>
2022-08-29 04:39:42 -0300

    Export score

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -12,4 +12,10 @@ public class BlockController : MonoBehaviour {
 
         gameObject.SetActive(false);
     }
+
+    private void OnDisable() {
+        if(LevelManager.OnBlockDestroyed != null) {
+            LevelManager.OnBlockDestroyed();
+        }
+    }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,14 +1,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
+    [SerializeField] private List<GameObject> _blocks;
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _ball;
     [SerializeField] private GameObject _gameOver;
 
+    public static Action OnBlockDestroyed;
+
+    private void ScoreExport(int score) {
+        string path = Application.persistentDataPath + "/score.txt";
+        Debug.Log(path);
+        
+        if(!File.Exists(path)) {
+            File.WriteAllText(path, score + "\n");
+
+            return;
+        }
+
+        File.AppendAllText(path, score + "\n");
+    }
+
+    private void BlockDestroyed() {
+        foreach(var block in _blocks) {
+            if(block.activeSelf) {
+                return;
+            }
+        }
+
+        // Win
+        // Export score
+        _player.SetActive(false);
+        _ball.SetActive(false);
+
+        ScoreExport(PlayerController.Score);
+    }
+
     public void RestartButton() {
+        
+
         _gameOver.SetActive(false);
 
         _player.SetActive(true);
@@ -23,6 +57,7 @@ public class LevelManager : MonoBehaviour {
             _gameOver.SetActive(true);
 
             // Export score
+            ScoreExport(PlayerController.Score);
 
             PlayerController.Score = PlayerController.Config.DEFAULT_START_SCORE;
             PlayerController.Life = PlayerController.Config.DEFAULT_START_LIFE;
@@ -36,9 +71,11 @@ public class LevelManager : MonoBehaviour {
 
     private void OnEnable() {
         DeathZoneController.OnDeath += RestartLevel;
+        OnBlockDestroyed += BlockDestroyed;
     }
 
     private void OnDisable() {
         DeathZoneController.OnDeath -= RestartLevel;
+        OnBlockDestroyed -= BlockDestroyed;
     }
 }
