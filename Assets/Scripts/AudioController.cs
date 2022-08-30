028bf2f9ee189ceb9c90f4809913ab72ad5202f6
Mateus <correia.mateus@hotmail.com>
2022-08-29 21:27:42 -0300

    SFX

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class AudioController : MonoBehaviour {
     [SerializeField] private AudioSource _audioSourceBG;
     [SerializeField] private AudioSource _audioSourceFX;
-    [SerializeField] private List<AudioClip> _backgroundMusics;
+    [SerializeField] private AudioClip _backgroundMusic;
+    [SerializeField] private AudioClip _SFX;
 
-    public void FXBlockDestroyed() {
-        
+    private void FXBlockDestroyed() {
+        _audioSourceFX.PlayOneShot(_SFX);
     }
 
     private void Start() {
-        _audioSourceBG.clip = _backgroundMusics[Random.Range(0, _backgroundMusics.Count)];
+        _audioSourceBG.clip = _backgroundMusic;
         _audioSourceBG.Play();
+    }
+
+    private void OnEnable() {
+        LevelManager.OnBlockDestroyed += FXBlockDestroyed;
+    }
+
+    private void OnDisable() {
+        LevelManager.OnBlockDestroyed -= FXBlockDestroyed;
     }
 }
--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 
 * Life System
 * Score System
-* Background Music (Dance, Don't Delay by http://www.twinmusicom.org/)
+* Background Music(Dance, Don't Delay by http://www.twinmusicom.org/)
 * Final Score Export(txt)
+* SFX(Large Bubble Rise by https://www.freesfx.co.uk/)
